7aec2d98dc79792c4f10c2f1fbc6198287ed98dd
Jay Patel <jaympatel9294@gmai.com>
2020-04-09 00:09:58 +0530

    working with npm cors

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const cookieParser = require("cookie-parser")
 const cors = require("cors")
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: "null",
+    credentials: true
+}))
 
 
 app.use(express.json())
